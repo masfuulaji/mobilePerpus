1db2eb61cdba99f1150dfe849e2000e2bdc4d737
masfuulaji <masfuulaji@gmail.com>
2022-03-26 16:14:54 +0700

    import image to component

---
+++ src/assets/image/index.js
@@ -0,0 +1,2 @@
+import Logo from './shipit.jpg';
+export {Logo};
--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -1,5 +1,6 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import React, {useEffect} from 'react';
+import Logo from '../../assets/image/shipit.jpg';
 
 export default function Splash(props) {
   useEffect(() => {
@@ -8,10 +9,14 @@ export default function Splash(props) {
     }, 1000);
   });
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={styles.pages}>
+      <Image source={Logo} style={styles.imgLogo} />
       <Text>Splash Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  pages: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  imgLogo: {width: 100, height: 100, borderRadius: 60},
+});
